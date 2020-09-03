4478b5b211503853c18e9f1a2e602f1dc4e29bd2
Goldenbwuoy <mumanikidzwag@gmail.com>
2020-09-03 14:29:37 +0800

    Initial commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'a84aa9f1ad3c4ddae3b366dff30de985'
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/'

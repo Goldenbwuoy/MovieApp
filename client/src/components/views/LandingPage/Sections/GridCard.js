9ab28115738de56360b8f708a25ec78a94485ba2
Goldenbwuoy <mumanikidzwag@gmail.com>
2020-09-03 16:15:02 +0800

    Display movie info and toggle cast

--- client/src/components/views/LandingPage/Sections/GridCard.js
+++ client/src/components/views/LandingPage/Sections/GridCard.js
@@ -1,16 +1,36 @@
-import React from 'react'
-import { Col } from 'antd'
+import React from "react";
+import { Col } from "antd";
 
 function GridCard(props) {
+  if (props.actor) {
     return (
+      <div>
         <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
-                <a href={`/movie/${props.movieId}`}>
-                    <img style={{ width: '100%', height: '320px'}} src={props.image} alt="img" />
-                </a>
-            </div>
+          <div style={{ position: "relative" }}>
+              <img
+                style={{ width: "100%", height: "320px" }}
+                src={props.image}
+                alt="img"
+              />
+          </div>
         </Col>
-    )
+      </div>
+    );
+  } else {
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <div style={{ position: "relative" }}>
+          <a href={`/movie/${props.movieId}`}>
+            <img
+              style={{ width: "100%", height: "320px" }}
+              src={props.image}
+              alt="img"
+            />
+          </a>
+        </div>
+      </Col>
+    );
+  }
 }
 
-export default GridCard
+export default GridCard;
--- client/src/components/views/MovieDatail/MovieDetailPage.js
+++ client/src/components/views/MovieDatail/MovieDetailPage.js
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { API_URL, API_KEY, IMAGE_URL } from "../../Config";
+import MainImage from "../LandingPage/Sections/MainImage";
+import { Descriptions, Row, Button } from "antd";
+import GridCard from "../LandingPage/Sections/GridCard";
 
-function MovieDetailPage() {
-    return (
-        <div>
-            
+function MovieDetailPage(props) {
+  const [Movie, setMovie] = useState([]);
+  const [Crews, setCrews] = useState([]);
+  const [ActorToggle, setActorToggle] = useState(false);
+
+  useEffect(() => {
+    const movieId = props.match.params.movieId;
+    fetch(`${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`)
+      .then((response) => response.json())
+      .then((response) => {
+        setMovie(response);
+
+        fetch(`${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`)
+          .then((response) => response.json())
+          .then((response) => {
+            setCrews(response.cast);
+          });
+      });
+  }, []);
+
+  const handleClick = () => {
+    setActorToggle(!ActorToggle);
+  };
+
+  return (
+    <div>
+      {/* Movie main image */}
+      {Movie && (
+        <MainImage
+          image={`${IMAGE_URL}/w1280${Movie.backdrop_path}`}
+          title={Movie.original_title}
+          text={Movie.overview}
+        />
+      )}
+
+      {/* body */}
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button>Add to Favorite</Button>
         </div>
-    )
+      </div>
+
+      {/* Movie info table */}
+      <Descriptions title="Movie Info" bordered>
+        <Descriptions.Item label="Title">
+          {Movie.original_title}
+        </Descriptions.Item>
+        <Descriptions.Item label="release_date">
+          {Movie.release_date}
+        </Descriptions.Item>
+        <Descriptions.Item label="revenue">{Movie.revenue}</Descriptions.Item>
+        <Descriptions.Item label="runtime">{Movie.runtime}</Descriptions.Item>
+        <Descriptions.Item label="vote_average" span={2}>
+          {Movie.vote_average}
+        </Descriptions.Item>
+        <Descriptions.Item label="vote_count">
+          {Movie.vote_count}
+        </Descriptions.Item>
+        <Descriptions.Item label="status">{Movie.status}</Descriptions.Item>
+        <Descriptions.Item label="popularity">
+          {Movie.popularity}
+        </Descriptions.Item>
+      </Descriptions>
+
+      <br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button onClick={handleClick}>Toggle Actor View</Button>
+      </div>
+      <br/>
+
+      {/* Grid cards for crews */}
+
+      {ActorToggle && (
+        <Row gutter={[16, 16]}>
+          {Crews &&
+            Crews.map((crew, index) => (
+              <React.Fragment key={index}>
+                {crew.profile_path && (
+                  <GridCard
+                    actor
+                    image={`${IMAGE_URL}w500${crew.profile_path}`}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+        </Row>
+      )}
+    </div>
+  );
 }
 
-export default MovieDetailPage
+export default MovieDetailPage;
